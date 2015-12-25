636a7ae403ccbae8c1465d5800a63325189af8d3
Intern <intern@Naoki-no-MacBook-Pro.local>
2015-12-25 17:30:27 +0900

    Fix demo

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
 var t = require('./');
-//t.on('playing', function(data){ console.dir(data);} );
-//t.on('paused', function(data){ console.log('paused');} );
-t.play("sub/objective");
+t.on('playing', function(data){ console.dir(data);} );
+t.on('paused', function(data){ console.log('paused');} );
+t.play("");
